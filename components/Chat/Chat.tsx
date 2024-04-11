ec421578e560de55716d5b8cadca13521bfa1f2a
zJ <19760191+zJuuu@users.noreply.github.com>
2024-04-11 16:05:12 +0200

    remove missing type

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -2,7 +2,6 @@ import { Conversation, Message } from '@/types/chat';
 import { KeyValuePair } from '@/types/data';
 import { ErrorMessage } from '@/types/error';
 import { LLM, LLMID } from '@/types/llms';
-import { Plugin } from '@/types/plugin';
 import { Prompt } from '@/types/prompt';
 import { throttle } from '@/utils';
 import { IconArrowDown, IconClearAll, IconSettings } from '@tabler/icons-react';
