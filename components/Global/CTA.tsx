0c2f952befad99e0dc316d92f3b46822d54a1c5b
zJ <19760191+zJuuu@users.noreply.github.com>
2024-04-18 21:31:36 +0200

    update CTA

--- components/Global/CTA.tsx
+++ components/Global/CTA.tsx
@@ -3,11 +3,11 @@ import { FC } from "react";
 
 export const CTAButton: FC = () => {
     return (
-        <Link href="https://akashnet.typeform.com/to/rhR4cWxQ">
+        <Link href="https://console.akash.network">
             <button
-                className={`text-xs sm:text-sm font-semibold text-black dark:text-gray-100 cursor-pointer rounded-lg p-2 sm:p-3 transition-colors duration-200 hover:bg-[#ffb2b2]/90 bg-[#ce4747]`}
+                className={`text-xs sm:text-sm font-semibold text-gray-100 cursor-pointer rounded-lg p-2 sm:p-3 transition-colors duration-200 hover:bg-[#ffb2b2]/90 bg-[#ce4747]`}
             >
-                Want to run your own models?
+                Deploy AI models on Akash
             </button>
         </Link>
     );
