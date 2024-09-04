0a6de56dc98e4650364b7d2466902d9862bff386
zJ_ <19760191+zJuuu@users.noreply.github.com>
2024-09-04 23:51:18 +0200

    chore: Update Chat component styles for responsive layout

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -165,7 +165,7 @@ export const Chat: FC<Props> = memo(
             >
               {conversation.messages.length === 0 ? (
                 <>
-                  <div className="mx-auto flex w-[350px] flex-col space-y-10 pt-12 sm:w-[600px]">
+                  <div className="mx-auto flex w-[350px] flex-col space-y-10 md:pt-12 sm:w-[600px]">
                     <div className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
                       {models.length === 0 ? (
                         <div>
